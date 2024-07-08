fadaab09a01d82e6a4379ea68359f5c21f1f55c0
gzalz <ericdaniel.gonzalez@gmail.com>
2024-07-08 10:10:50 -0600

    update release-rom.sh

--- snake-gba/release-rom.sh
+++ snake-gba/release-rom.sh
@@ -1,4 +1,5 @@
-cargo build --examples --release
+cargo build --release
 
 arm-none-eabi-objcopy -O binary target/thumbv4t-none-eabi/release/snake target/snake.gba
 gbafix -p -thello -cHELO -mRS target/snake.gba
+mv target/snake.gba rom/
